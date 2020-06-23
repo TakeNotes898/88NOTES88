cafda767442a76cc7e1925d1cda60742183965cd
sxena <s_xena@hotmail.com>
2020-06-23 16:27:02 -0700

    some intersting stuff

--- 00SuperChallenge02try/js.js
+++
@@ -1,8 +0,0 @@
-// EPIC Challenge
-// High/Low Game
-// Create an HTML page and link your JavaScript file
-// More information about linking HTML to JavaScript here
-// As a user, I see a prompt or input where I can guess a number between 1 and 100 (both inclusive)
-// As a user, when I see if my guess is too high or too low
-// As a user, if I guess the number correctly I am notified that I won
-// STRETCH: As a user, if I have not guessed the correct number in seven tries I see a losing message
