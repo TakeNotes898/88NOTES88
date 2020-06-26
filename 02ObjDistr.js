5c37166bcd9d956c7609eab6050cc59b765035f0
sxena <s_xena@hotmail.com>
2020-06-26 11:03:36 -0700

    dustructure in motion

--- 02ObjDistr.js
+++ 02ObjDistr.js
@@ -73,13 +73,57 @@ console.log(ingredients[2]);
 // *****************************************
 var newFunkLunch = {
   makeASandwich: function(){
-    return `The ingredients for a ${name} ${type} ${ingredients}.`
+    return `The ingredients for a ${name} ${type} ${ingredients[0]}, ${ingredients[1]} and ${ingredients[2]}.`
   }
 }
 lunch = {...lunch, ...newFunkLunch}
-// var {newFunkLunch} = lunch;
 console.log(lunch);
 console.log(lunch.makeASandwich());
+
+// *****************************************
+// *****************************************
+// *****************************************
+var animals = [
+  { name1: "Waffles", type1: "dog", age: 12 },
+  { name1: "Fluffy", type1: "cat", age: 14 },
+  { name1: "Spelunky", type1: "dog", age: 4 },
+  { name1: "Hank", type1: "cat", age: 11 },
+]
+// Create a function that takes in any array of objects
+// and returns a new array with only those of type cat.
+var caties = animals.filter(value => value.type1 === "cat")
+console.log(caties);
+// Using the same array of objects above.
+// Create a function that returns a new array
+// with only the names of the animals.
+
+let newTypeOfArray = {
+  onlyNames: function() {
+    return `bla bla bla ${name1}`
+    // animals.filter(value => value.animals === animals.name1)
+  }
+}
+animals = {...animals, ...newTypeOfArray}
+console.log(animals);
+// console.log(animals.onlyNames());
+({ name1 } = animals);
+console.log(name1);
+
+
+
+// *****************************************
+// *****************************************
+// *****************************************
+
+
+
+
+
+
+
+
+
+
 
 
 // *****************************************
