f4453d5a534c0396b521e657c5ed65f1f789e1e2
sxena <s_xena@hotmail.com>
2020-06-29 11:16:32 -0700

    added a third react app, just some simple stuff to jogg the memory

---
+++ 03 React/react-intro-03/src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+
+class Footer extends Component{
+  render(){
+    return(
+      <footer className="App">
+
+        <p>The TEAM name is GoDolphins</p>
+
+      </footer>
+  );
+}
+};
+export default Footer;
