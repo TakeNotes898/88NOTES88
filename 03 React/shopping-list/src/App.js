50c144a3bb4bfc57dd24ff56ed9fb07981cf85d4
sxena <s_xena@hotmail.com>
2020-07-11 15:18:19 -0700

    added some items to the shopping list

--- 03 React/shopping-list/src/App.js
+++ 03 React/shopping-list/src/App.js
@@ -3,11 +3,19 @@ import Item from './components/Item.js'
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      listItems: [ 'apples', 'yogurt', 'pineapple', 'milk', 'bluberries', 'ice cream' ],
+      hardWareItems: ['lightbulb' , 'hammer', 'axe', 'nails']
+    }
+  }
   render(){
     return(
       <React.Fragment>
         <h1>To Do List</h1>
-        <Item listItem = "apples" />
+        <Item list = {this.state.listItems} />
+        <Item list = {this.state.hardWareItems} />
 
 
 
