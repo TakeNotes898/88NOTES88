fcc176d3f79b5d593f4ac1729ca16ef76040fe38
sxena <s_xena@hotmail.com>
2020-07-13 21:27:49 -0700

    rrrr

--- 03 React/dice-roller/src/components/Dice.js
+++ 03 React/dice-roller/src/components/Dice.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+// import Image from 'react-bootstrap/Image'
 // import one from './img/01.jpg';
 // import two from './img/02.jpg';
 // import three from './img/03.jpg';
@@ -10,42 +11,46 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 // import six from './img/06.jpg';
 
 
-
-
 class Dice extends Component{
 
 
 
 rollTheDice = () => {
-  let theDice = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg'];
+  let theDice = [1, 2, 3, 4, 5, 6];
   let random = Math.floor(Math.random() * theDice.length)
   this.props.randomNumberRolled(theDice[random])
 }
-
-
 
 
   render(){
     return(
       <React.Fragment>
 
-      <Card bg="dark" text="white" className="text-center p-5">
-
-      <ButtonGroup className="mr-1">
-      <p><Button type="submit" size="lg"  variant="primary" onClick={ this.rollTheDice}>RoLL</Button></p>
-      </ButtonGroup>
-
-      <ButtonGroup>
-      <p><Button size="lg"  type = "reset" variant="danger">Reset</Button></p>
-      </ButtonGroup>
-
-        <blockquote className="blockquote mb-0 card-body">
-
-          <h1>{ this.props.numberRolled }</h1>
+          <Card className="text-center p-3">
+            <Card.Body>
+              <Card.Title>What Can you Do Here?</Card.Title>
+              <ButtonGroup className="mr-1"><p><Button type="submit" size="lg"  variant="primary" onClick={ this.rollTheDice}>RoLL</Button></p></ButtonGroup>
+              <ButtonGroup><p><Button size="lg"  type="reset" variant="danger">Reset</Button></p></ButtonGroup>
+              <ButtonGroup><p><Button size="lg" variant="warning"><a href="http://xenadev.com" target="_blank">XenaDev</a></Button></p></ButtonGroup>
+            </Card.Body>
+          </Card>
 
 
-        </blockquote>
-      </Card>
+          <Card bg="dark" text="white" className="text-center p-5">
+            <blockquote className="blockquote mb-0 card-body">
+              <h1>{ this.props.numberRolled }</h1>
+            </blockquote>
+          </Card>
+
+
+          <Card className="text-center p-3">
+            <Card.Body>
+              <Card.Title>What Have we rolled</Card.Title>
+              <Card.Text>
+                you need to put here the code that will record what we have rolled
+              </Card.Text>
+            </Card.Body>
+          </Card>
 
 
       </React.Fragment>
--- 03 React/dice-roller/src/components/Right.js
+++ 03 React/dice-roller/src/components/Right.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 class Right extends Component{
 
@@ -10,15 +8,7 @@ class Right extends Component{
     return(
       <React.Fragment>
 
-      <Card className="text-center p-3">
-        <Card.Body>
-          <Card.Title>What Have we rolled</Card.Title>
-          <Card.Text>
-            you need to put here the code that will record what we have rolled
-          </Card.Text>
 
-        </Card.Body>
-      </Card>
 
 
       </React.Fragment>
