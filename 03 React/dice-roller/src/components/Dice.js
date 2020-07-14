3b6bbe9268f2af7db59b986724cf1d06e8ac02e0
sxena <s_xena@hotmail.com>
2020-07-14 15:04:17 -0700

    with some brain work the img is rendering but the reslts are still not being recorded snifl

--- 03 React/dice-roller/src/components/Dice.js
+++ 03 React/dice-roller/src/components/Dice.js
@@ -3,24 +3,36 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 // import Image from 'react-bootstrap/Image'
-// import one from './img/01.jpg';
-// import two from './img/02.jpg';
-// import three from './img/03.jpg';
-// import four from './img/04.jpg';
-// import five from './img/05.jpg';
-// import six from './img/06.jpg';
+import one from '../img/01.jpg';
+import two from '../img/02.jpg';
+import three from '../img/03.jpg';
+import four from '../img/04.jpg';
+import five from '../img/05.jpg';
+import six from '../img/06.jpg';
 
 
 class Dice extends Component{
+  constructor(){
+    super();
+    this.state={
+      numberHolder : 2
 
+     }
+    }
 
-
-rollTheDice = () => {
-  let theDice = [1, 2, 3, 4, 5, 6];
-  let random = Math.floor(Math.random() * theDice.length)
-  this.props.randomNumberRolled(theDice[random])
+diceImg = (imgs) => {
+    let theDice = [one, two, three, four, five, six]
+    return theDice[imgs]
 }
 
+rollTheDice = () => {
+  let random = Math.floor(Math.random() * 6)
+  this.setState({ numberHolder : random, imgs : this.props.numRecorded.concat([random])})
+}
+
+reset = () => {
+        window.location.reload()
+    }
 
   render(){
     return(
@@ -29,25 +41,26 @@ rollTheDice = () => {
           <Card className="text-center p-3">
             <Card.Body>
               <Card.Title>What Can you Do Here?</Card.Title>
-              <ButtonGroup className="mr-1"><p><Button type="submit" size="lg"  variant="primary" onClick={ this.rollTheDice}>RoLL</Button></p></ButtonGroup>
-              <ButtonGroup><p><Button size="lg"  type="reset" variant="danger">Reset</Button></p></ButtonGroup>
-              <ButtonGroup><p><Button size="lg" variant="warning"><a href="http://xenadev.com" target="_blank">XenaDev</a></Button></p></ButtonGroup>
+              <ButtonGroup className="mr-1"><p><Button type="submit" size="sm"  variant="primary" onClick={ this.rollTheDice}>RoLL</Button></p></ButtonGroup>
+              <ButtonGroup className="mr-1"><p><Button size="sm"  onClick = {this.reset} variant="danger">Reset</Button></p></ButtonGroup>
+              <ButtonGroup><p><Button size="sm" variant="warning"><a href="http://xenadev.com" target="_blank">XenaDev</a></Button></p></ButtonGroup>
             </Card.Body>
           </Card>
 
 
-          <Card bg="dark" text="white" className="text-center p-5">
+          <Card bg="dark" text="white" className="text-center p-3">
             <blockquote className="blockquote mb-0 card-body">
-              <h1>{ this.props.numberRolled }</h1>
+              <h1>{ this.props.numberRolled }
+              <img width='100%' src = { this.diceImg(this.state.numberHolder) }/></h1>
             </blockquote>
           </Card>
-
 
           <Card className="text-center p-3">
             <Card.Body>
               <Card.Title>What Have we rolled</Card.Title>
               <Card.Text>
-                you need to put here the code that will record what we have rolled
+
+                  <ol>{this.props.numRecorded.map(value => <li>{value}</li>)}</ol>
               </Card.Text>
             </Card.Body>
           </Card>
